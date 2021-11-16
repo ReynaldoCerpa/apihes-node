f6381514c99952e82997eca4ba461e5092f5e364
ReynaldoCerpa <reynaldocerpa07@gmail.com>
2021-11-16 10:21:20 -0800

    first commit

---
+++ src/controllers/index.controller.ts
@@ -0,0 +1,17 @@
+import {Request, Response} from "express";
+import { connect } from '../database'
+const path = require("path");
+
+export async function getPromotores(req: Request, res: Response): Promise<Response | void> {
+    try {
+        console.log("Connected");
+        
+        const conn = await connect();
+        const posts = await conn.query('SELECT * FROM promotor');
+        return res.json(posts[0]);
+    }
+    catch (e) {
+        console.log(e)
+    }
+}
+
---
+++ src/routes/login.routes.ts
@@ -0,0 +1,10 @@
+import {Router} from "express";
+const router = Router();
+
+import * as login from "../controllers/login.controller"
+
+router.route("/login")
+    .post(login.loginPage)
+
+
+export default router;
