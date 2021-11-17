265dee469751ef94db6574b496b8a4b043e1132b
Carlos M <carlosedm@protonmail.com>
2021-11-17 11:15:38 -0700

    fix hopefully

--- src/controllers/dashboard.controller.ts
+++ src/controllers/dashboard.controller.ts
@@ -7,7 +7,7 @@ export async function getDashboardData(req: Request, res: Response): Promise<Res
         console.log("Connected");
         
         const conn = await connect();
-        const data = await conn.query('select idReporte, MONTH(fechaElaboracion), fechaElaboracion, idReporte, titulo, idReporte, idReporte, idReporte from reporteidea');
+        const data = await conn.query("select idReporte as 'numProp', MONTH(fechaElaboracion) as 'mes', fechaElaboracion as 'fecha', idReporte as 'areaPropone', titulo as 'titulo', idReporte as 'areaMejora', idReporte as 'tipoMejora', idReporte as 'gerente', from reporteidea");
         return res.json(data);
     }
     catch (e) {
